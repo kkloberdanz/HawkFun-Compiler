a51b9f2be59fe214de424d61d926cffe71bc4289
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-30 19:01:52 -0600

    first commit

---
+++ Project/Env.fs
@@ -0,0 +1,13 @@
+(* File Project/Env.fs
+ *)
+
+module Env
+
+(* Environment operations *)
+
+type 'v env = (string * 'v) list
+
+let rec lookup env x =
+    match env with 
+    | []          -> failwith (x + " not found in environment")
+    | (y, v) :: r -> if x = y then v else lookup r x
---
+++ Project/Inter.fs
@@ -0,0 +1,34 @@
+(* File Project/Inter.fs
+*)
+
+module Inter
+
+open Absyn
+open Env
+
+
+(* A runtime value is an integer, a list, or a function closure *)
+
+type value = 
+  | Int of int
+  | List of value list
+  | Closure of string option * string * expr * value env   
+
+
+let rec toString v t = 
+  match (v, t) with
+  | (_, AnyT)          -> "value" 
+  | (_, ArrowT (_, _)) -> "closure"  
+  | (_, UnitT)         -> "null"
+  | (Int i, IntT)      -> string i
+  | (Int 0, BoolT)     -> "false"
+  | (Int 1, BoolT)     -> "true"
+  | (List l, ListT t1) -> "[" + listToString l t1 + "]"
+  | _ ->  failwith "toString: mismatched type and value"
+and listToString l t =
+  match l with
+  |      [] -> ""
+  | v :: [] -> toString v t
+  | v :: vs -> (toString v t) + "; " + (listToString vs t)
+
+
